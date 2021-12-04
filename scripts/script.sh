11da1b8b85f0302f0d913f677017662a7e6a0ef0
aylwing-ww <moisesalex132@gmail.com>
2021-12-04 09:31:01 -0700

    TEST: file test from buildkite

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,2 +1,9 @@
 set -euo pipefail
-echo "Hello from bash"
+
+echo "+++ :hammer: Example tests"
+
+echo -e "\033[33mCongratulations!\033[0m You've successfully run your first build on Buildkite! 👍
+\033[33m$(cat artifacts/thumbsup.txt)\033[0m
+If you have any questions or need help email support@buildkite.com, we'd be happy to help!
+\033[31m<3\033[0m Buildkite
+"
